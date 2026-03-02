c65e4fa6000d50f7772b10cc3c0308b915e63fbc
uselessmorning <shtoykoigor@ukr.net>
2026-03-02 22:12:27 +0100

    question

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
 // write code here
+
+const tree = document.querySelector('.tree');
+const lists = tree.querySelectorAll('ul');
+
+lists.forEach((ul) => {
+  ul.hidden = true;
+});
+
+tree.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const parentLi = e.target.closest('li');
+  const nestedList = parentLi.querySelector('ul');
+
+  if (!nestedList) {
+    return;
+  }
+
+  nestedList.hidden = !nestedList.hidden;
+});
